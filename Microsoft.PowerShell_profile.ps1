40dde7245113c6d0ad4e034cce338f1b561ab89c
Baptiste LECHAT <baptistelechat@outlook.fr>
2022-04-01 12:11:55 +0200

    🎉 First Commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,5 @@
+# PATH → C:\Users\{username}\Documents\WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+oh-my-posh init pwsh | Invoke-Expression
+Set-PoshPrompt -Theme bubblesextra
+Import-Module -Name Terminal-Icons
+
---
+++ install.ps1
@@ -0,0 +1,4 @@
+winget install JanDeDobbeleer.OhMyPosh
+Install-Module oh-my-posh -Scope CurrentUser -Force
+Install-Module -Name Terminal-Icons -Repository PSGallery -Scope CurrentUser -Force
+
