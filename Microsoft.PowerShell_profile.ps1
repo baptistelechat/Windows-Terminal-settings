61c021a507abf7ae60ee5dec64cc3d8c576fab8f
Baptiste LECHAT <baptistelechat@outlook.fr>
2022-04-01 18:47:00 +0200

    📝 Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
 # PATH → C:\Users\{username}\Documents\WindowsPowerShell/Microsoft.PowerShell_profile.ps1
 # oh-my-posh init pwsh | Invoke-Expression
-oh-my-posh init pwsh --config ~/Desktop/DEV/.myBubblesextraTheme.omp.json | Invoke-Expression
+oh-my-posh init pwsh --config ~/Documents/WindowsPowerShell/myBubblesextraTheme.omp.json | Invoke-Expression
 # Set-PoshPrompt -Theme bubblesextra
 Import-Module -Name Terminal-Icons
